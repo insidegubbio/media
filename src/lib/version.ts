594dfa6ef959ce7eaf7acaa60e17bd17174824a6
diced <git@diced.sh>
2025-05-06 01:19:08 -0700

    fix: import issue

--- src/lib/version.ts
+++ src/lib/version.ts
@@ -1,13 +1,13 @@
 // TODO: implement version checking against the API at https://zipline.diced.sh
 
 import { version } from '../../package.json';
+import { execSync } from 'child_process';
 
 export function gitSha() {
   const envValue = process.env.ZIPLINE_GIT_SHA;
   if (envValue && envValue !== 'unknown') return envValue;
 
   try {
-    const { execSync } = require('child_process');
     const commitHash = execSync('git rev-parse --short HEAD').toString().trim();
     return commitHash;
   } catch (error) {
