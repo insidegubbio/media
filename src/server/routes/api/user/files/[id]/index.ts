b2db0c15a3ff3356e570193f0bfdf8eb92753ef5
diced <git@diced.sh>
2025-11-15 23:20:11 -0800

    fix: editing files that are owned by current user

--- src/server/routes/api/user/files/[id]/index.ts
+++ src/server/routes/api/user/files/[id]/index.ts
@@ -39,7 +39,8 @@ export default fastifyPlugin(
       });
       if (!file) return res.notFound();
 
-      if (!canInteract(req.user.role, file.User?.role ?? 'USER')) return res.notFound();
+      if (req.user.id !== file.User?.id && !canInteract(req.user.role, file.User?.role ?? 'USER'))
+        return res.notFound();
 
       return res.send(file);
     });
@@ -56,7 +57,8 @@ export default fastifyPlugin(
       });
       if (!file) return res.notFound();
 
-      if (!canInteract(req.user.role, file.User?.role ?? 'USER')) return res.notFound();
+      if (req.user.id !== file.User?.id && !canInteract(req.user.role, file.User?.role ?? 'USER'))
+        return res.notFound();
 
       const data: Prisma.FileUpdateInput = {};
 
